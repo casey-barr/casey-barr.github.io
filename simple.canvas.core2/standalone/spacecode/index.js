4e9ccf07bdbd7701be14843d0a0d0eca44ed94a0
Casey C. Barr <49072955+casey-barr@users.noreply.github.com>
2021-08-18 08:47:20 -0400

    Create index.js

---
+++ simple.canvas.core2/standalone/spacecode/index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function sceneSelectByHashValue(){
+        let sceneName = location.hash.replace('#','');
+        if(sceneName == ''){
+            return;
+        }
+        
+        if(SCG.scenes.cachedScenes[sceneName] != undefined)
+            SCG.scenes.selectScene(sceneName);
+        else 
+            return;
+    }
+
+    SCG.globals.version = 0.1;
+
+    SCG.src = {
+        code: CodeCatScene.models.code
+    }
+
+    
+    SCG.scenes.cacheScene(new SpaceshipScene({
+        name:'spaceship',
+        viewport: new V2(200, 200)
+    }));
+
+    SCG.scenes.selectScene('spaceship0');
+    
+    SCG.main.start();
+});
